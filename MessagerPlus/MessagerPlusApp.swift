b5e8ca0aba5868e759e1fe622dba6ecf3779e192
huyhdq <huyhdq@ftech.ai>
2023-08-01 09:15:39 +0700

    Initial Commit

---
+++ MessagerPlus/MessagerPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessagerPlusApp.swift
+//  MessagerPlus
+//
+//  Created by Ha Duyen Quang Huy on 01/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MessagerPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
